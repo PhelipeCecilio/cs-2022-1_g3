af16b1753bed4cd256379e5b40d0f6c5cf04758b
Arthur <arthurcastroes@gmail.com>
2022-08-17 21:13:02 -0300

    refactor: auth controller with login

---
+++ backend/src/controllers/AuthController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { prisma } from "../database/prismaClient";
+import jwt from "jsonwebtoken";
+
+import bcrypt from "bcrypt";
+
+export class AuthController {
+  static async login(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await prisma.user.findUnique({ where: { email } });
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      if (user) {
+        if (await bcrypt.compare(password, user.password)) {
+          const token = jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET as string,
+            {
+              expiresIn: "24h",
+            }
+          );
+
+          return res.json({ token });
+        } else {
+          return res.status(400).json({ message: "Invalid password" });
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+}
