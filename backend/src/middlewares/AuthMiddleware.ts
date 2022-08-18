dbcb1dc754d937fa0ca49a86c786c37e95c5dc90
Arthur <arthurcastroes@gmail.com>
2022-08-17 21:12:48 -0300

    refactor: auth middleware

--- backend/src/middlewares/AuthMiddleware.ts
+++ backend/src/middlewares/AuthMiddleware.ts
@@ -1,59 +1,33 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { NextFunction } from "express";
-import { UserController } from "../controllers/UserController";
-import bcrypt from "bcrypt";
 
 export class AuthMiddleware {
-  constructor() {}
-
-  static async login(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { email, password } = req.body;
-      const user = await UserController.authenticate(email);
-      if (!user) {
-        return res.json({ message: "User not found" });
-      }
-      console.log(user);
-      if (user) {
-        if (await bcrypt.compare(password, user.password)) {
-          const token = jwt.sign(
-            { id: user.id },
-            process.env.JWT_SECRET as string,
-            {
-              expiresIn: "24h",
-            }
-          );
-          console.log(token);
-          return res.json({ token });
-        } else {
-          console.log("Senha incorreta");
-          return res.json({ message: "Invalid password" });
-        }
-      }
-      next(user);
-    } catch (error) {
-      next(error);
-      console.log(error);
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-  }
-
-  static async authenticate(req: Request, res: Response, next: NextFunction) {
+  static async isAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
     try {
       const token = req.headers.authorization;
+
       if (!token) {
-        return res.json({ message: "Token not found" });
+        return res.status(404).json({ message: "Token not found" });
       }
-      const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+
+      const parsedToken = token.split(" ")[1];
+
+      const decoded = jwt.verify(parsedToken, process.env.JWT_SECRET as string);
+
       if (!decoded) {
         return res.json({ message: "Invalid token" });
       }
+
       next();
     } catch (error) {
       next(error);
       console.log(error);
-      res.status(500).json({ message: "Internal Server Error" });
+
+      return res.status(500).json({ message: "Internal Server Error" });
     }
   }
 }
