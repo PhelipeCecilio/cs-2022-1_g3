8c28502f128fb40050d524ae756c2f2304b3666c
Arthur <arthurcastroes@gmail.com>
2022-08-17 21:13:30 -0300

    refactor: routes organization

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,56 +1,32 @@
 import express from "express";
 import { UserController } from "../controllers/UserController";
+import { AuthController } from "../controllers/AuthController";
 import { ChatController } from "../controllers/ChatController";
 import { MessageController } from "../controllers/MessageController";
 import { AuthMiddleware } from "../middlewares/AuthMiddleware";
 
 const routes = express.Router();
 
-routes.get("/", (req: express.Request, res: express.Response) => {
-  res.status(200).json({ message: "API Request" });
-});
-
-routes.post("/login", AuthMiddleware.login, UserController.getUser);
+routes.post("/login", AuthController.login);
 
 routes.post("/signup", UserController.createUser);
-routes.get("/user/:email", UserController.getUser);
+routes.get("/users/:email", UserController.getUser);
 routes.get("/users", UserController.listUsers);
-routes.put("/user/status", UserController.changeStatus);
 
-routes.post("/chat", AuthMiddleware.authenticate, ChatController.createChat);
-routes.get("/chat/:id", AuthMiddleware.authenticate, ChatController.getChat);
-routes.get("/chats", AuthMiddleware.authenticate, ChatController.listChats);
-routes.put("/chat/:id", AuthMiddleware.authenticate, ChatController.updateChat);
-routes.delete(
-  "/chat/:id",
-  AuthMiddleware.authenticate,
-  ChatController.deleteChat
-);
+// All routes below require authentication
+routes.use(AuthMiddleware.isAuthenticated);
 
-routes.post(
-  "/message",
-  AuthMiddleware.authenticate,
-  MessageController.createMessage
-);
-routes.get(
-  "/message/:id",
-  AuthMiddleware.authenticate,
-  MessageController.getMessage
-);
-routes.get(
-  "/messages",
-  AuthMiddleware.authenticate,
-  MessageController.listMessages
-);
-routes.put(
-  "/message/:id",
-  AuthMiddleware.authenticate,
-  MessageController.updateMessage
-);
-routes.delete(
-  "/message/:id",
-  AuthMiddleware.authenticate,
-  MessageController.deleteMessage
-);
+routes.put("/users/:email/status", UserController.changeStatus);
+routes.post("/chat", ChatController.createChat);
+routes.get("/chat/:id", ChatController.getChat);
+routes.get("/chats", ChatController.listChats);
+routes.put("/chat/:id", ChatController.updateChat);
+routes.delete("/chat/:id", ChatController.deleteChat);
+
+routes.post("/message", MessageController.createMessage);
+routes.get("/message/:id", MessageController.getMessage);
+routes.get("/messages", MessageController.listMessages);
+routes.put("/message/:id", MessageController.updateMessage);
+routes.delete("/message/:id", MessageController.deleteMessage);
 
 export default routes;
