4390332629ed81489ba256fc564031dd790fe149
Arthur <arthurcastroes@gmail.com>
2022-08-17 21:11:53 -0300

    refactor: required config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -2,15 +2,16 @@ import { config } from "dotenv";
 
 config();
 
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined");
+}
+
+if (!process.env.DB_HOST) {
+  throw new Error("DB_HOST is not defined");
+}
+
 export default {
   // application configurations
-  appName: process.env.APP_NAME || "",
+  appName: process.env.APP_NAME,
   appPort: process.env.APP_PORT || 3000,
-
-  // database configurations
-  databaseHost: process.env.DB_HOST || "",
-  databaseUser: process.env.DB_USER || "root",
-  databasePassword: process.env.DB_PASS || "",
-  databaseName: process.env.DB_NAME || "",
-  databasePort: process.env.DB_PORT || 3306,
 };
