3271522fa946c222968ab29791f528293519c03a
Arthur <arthurcastroes@gmail.com>
2022-08-17 22:06:25 -0300

    refactor: check if is admin

--- backend/src/middlewares/AuthMiddleware.ts
+++ backend/src/middlewares/AuthMiddleware.ts
@@ -1,3 +1,4 @@
+import { prisma } from "../database/prismaClient";
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
@@ -28,6 +29,51 @@ export class AuthMiddleware {
       console.log(error);
 
       return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+
+  static async isAdmin(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = req.headers.authorization;
+
+      if (!token) {
+        return res.status(404).json({ message: "Token not found" });
+      }
+
+      const parsedToken = token.split(" ")[1];
+
+      const decoded = jwt.verify(parsedToken, process.env.JWT_SECRET as string);
+
+      if (!decoded) {
+        return res.status(400).json({ message: "Invalid token" });
+      }
+
+      const userId = typeof decoded === "object" ? decoded.id : null;
+
+      if (!userId) {
+        return res.status(400).json({ message: "Invalid token" });
+      }
+
+      const user = await prisma.user.findUnique({ where: { id: userId } });
+
+      console.log(user?.name, user?.role);
+
+      if (!user) {
+        return res.status(400).json({ message: "Invalid user" });
+      }
+
+      if (user?.role !== "ADMIN") {
+        return res.status(401).json({
+          message: "Operation not allowed! Only admins can perform that.",
+        });
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+      console.log(error);
+
+      return res.status(500).json({ message: "Unable to verify token" });
     }
   }
 }
