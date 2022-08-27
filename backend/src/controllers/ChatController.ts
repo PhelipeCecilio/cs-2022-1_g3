93542983d06a4e8549cbc44d82263fd8c4420f6b
Arthur <arthurcastroes@gmail.com>
2022-08-27 18:00:16 -0300

    refactor: chats

--- backend/src/controllers/ChatController.ts
+++ backend/src/controllers/ChatController.ts
@@ -1,12 +1,42 @@
 import { Request, Response } from "express";
+
 import { prisma } from "../database/prismaClient";
+
+import * as Yup from "yup";
 
 export class ChatController {
   static async createChat(req: Request, res: Response) {
-    const name = req.body.chatName;
+    //@ts-ignore
+    console.log(req.currentUser);
+
+    let schema = Yup.object().shape({
+      name: Yup.string().required().min(3),
+    });
+
     try {
-      const chat = await prisma.chat.create({ data: { name } });
-      return res.json(chat);
+      await schema.validate(req.body);
+    } catch (err: any) {
+      console.log(err);
+      return res.status(400).json({ error: err });
+    }
+
+    const { name } = req.body;
+
+    const foundChat = await prisma.chat.findUnique({
+      where: { name },
+    });
+
+    if (foundChat) {
+      return res.status(400).json({ error: "Chat already exists" });
+    }
+
+    try {
+      const chat = await prisma.chat.create({
+        //@ts-ignore
+        data: { name, userId: req.currentUser.id },
+      });
+
+      return res.json({ chat });
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: "Internal Server Error" });
@@ -34,8 +64,14 @@ export class ChatController {
   }
 
   static async listChats(req: Request, res: Response) {
+    const userId = req.query.userId as string | undefined;
+
     try {
-      const chats = await prisma.chat.findMany();
+      const chats = await prisma.chat.findMany({
+        where: {
+          userId,
+        },
+      });
       return res.json(chats);
     } catch (error) {
       console.log(error);
@@ -45,6 +81,7 @@ export class ChatController {
 
   static async updateChat(req: Request, res: Response) {
     const id = req.params.id;
+
     try {
       let chat = await prisma.chat.findUnique({ where: { id } });
       if (!chat) {
